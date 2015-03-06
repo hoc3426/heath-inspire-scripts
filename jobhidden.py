dd03b857cef7b2bd1d518f38ffc982642e102c70
Heath O'Connell <hoc3426@gmail.com>
2015-03-06 21:00:47 +0100

    can now copy in whole line

--- jobhidden.py
+++ jobhidden.py
@@ -2,9 +2,10 @@ from invenio.bibrecord import print_rec, record_add_field, record_xml_output
 import re
 
 for i in open('tmp_jobhidden.in','r').readlines() :
-    matchObj = re.search("\d+", i)
+    matchObj = re.search("Remove posting in HEPJobs (\d+)", i)
     if matchObj : 
         recid = matchObj.group()
+        recid = re.sub(r'\D', r'', recid)
         common_fields = {}
         common_tags = {}
         record_add_field(common_fields, '001', controlfield_value=str(recid))
