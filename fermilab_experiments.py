20c74ddbe24abea7a46710b51dd66837456bb802
Heath O'Connell <hoc3426@gmail.com>
2018-04-07 17:40:12 +0200

    Rewritten completely

--- fermilab_experiments.py
+++ fermilab_experiments.py
@@ -1,149 +1,207 @@
 import os
 import re
 import time
-from invenio.search_engine import perform_request_search
-from invenio.search_engine import get_fieldvalues
-serieses = ['thesis','misc','tm','fn','proposal','workbook','bachelors','masters','design','loi','pbar','nal','annual','upc','ap','en','exp','lu','habil','vlhcpub','mura']
-serieses = ['experiments']
-serieses.sort()
+
+import lxml.html as LH
+import lxml.etree as ET
+
+from lxml.html.builder import ElementMaker, html_parser
+from BeautifulSoup import BeautifulSoup as BS
+from operator import itemgetter
+
+from invenio.search_engine import perform_request_search, \
+                                  get_fieldvalues
+from invenio.bibformat_engine import BibFormatObject as bfo
+
 print 'cd /web/sites/ccd.fnal.gov/htdocs/techpubs'
 
 VERBOSE = False
 #VERBOSE = True
 
-for series in serieses :
-  reports = []
-  authorId = False
-  search = "119__a:FNAL* or 419__a:FNAL*"
-  #search = "find recid 1110234"
-  #print search 
-  result = perform_request_search(p=search,cc='Experiments')
-  for recid in result : 
-    reportValues = get_fieldvalues(recid,'119__a')
-    reportValues += get_fieldvalues(recid,'419__a')
-    author = get_fieldvalues(recid,'702__a')
-    authorId = get_fieldvalues(recid,'702__i')
-    authorAff = get_fieldvalues(recid,'702__u')
-    title = get_fieldvalues(recid,'245__a')
-    collaboration = get_fieldvalues(recid,'710__g')
-    try:
-        end_date = get_fieldvalues(recid,'046__t')[0]
-    except: 
-        try:
-            end_date = get_fieldvalues(recid,'046__c')[0]
-        except:
-            end_date = ''       
-    if VERBOSE: print recid, reportValues, author, authorId, authorAff, title, collaboration, end_date
-    if author : 
-      author = author[0]    
-    else :
-      author = ''
-    if title : 
-      title = '<i>' + title[0][:100] + '</i>'
-    else :
-      title = ''
-    if collaboration :
-      collaboration = collaboration[0]
-    else :
-      collaboration = ''
-    if authorAff :
-      authorAff = authorAff[0]
-    else :
-      authorAff = ''
-    #print "author = ", author
-    #print "title = ",  title
-    #print "authorId = ",  authorId
-    #print "collaboration = ", collaboration
-    if authorId : 
-      authorId = authorId[0]
-    else:
-      authorId = ''
-    for report in reportValues :  
-      if re.match('FNAL-', report, re.IGNORECASE) \
-      or re.match(r'MURA-\d', report, re.IGNORECASE) \
-      or re.match(r'MURA-\w\-', report, re.IGNORECASE) \
-      or re.match(r'MURA-\w$', report, re.IGNORECASE):
-        search_proposal = re.sub(r'FNAL-\w-', r'FERMILAB-PROPOSAL-', report)
-        #search_proposal = 'find exp ' + report + ' and r proposal'
-        proposal_recid = perform_request_search(p=search_proposal,cc='HEP')
-        try:
-            proposal_recid = proposal_recid[0]
-            proposal_date = get_fieldvalues(proposal_recid,'269__c')[0]
-        except:
-            proposal_recid = ''
-            proposal_date = ''
-        sorter = re.sub(r'\D', '', report)
-        if re.search('FNAL-T', report):
-          title = '<font color="green">' + title + '</font>'
-        y = [sorter,report,str(recid),author,title,authorId,collaboration,authorAff,str(proposal_recid),proposal_date,end_date]
-        if VERBOSE: print "y = ", y
-        reports.append(y)
-  reports.sort(reverse=True)
+SEARCH = "119__a:/^FNAL-[EPT]-1/ or 419__a:/^FNAL-[EPT]-1/"
+SEARCH = "119__a:/^FNAL/ or 419__a:/^FNAL/"
+#SEARCH = "119__a:/^FNAL-E-0830/"
 
-  file = 'www/fermilab-reports-' + series + '.html' 
-  output = open(file,'w')
-  output.write('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"\n')
-  output.write('        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">\n')
-  output.write('<html xmlns="http://www.w3.org/1999/xhtml">\n')
-  output.write('<head>\n')
-  output.write('<title>Fermilab Technical Publications: ')
-  output.write(series)
-  output.write('</title>\n')
-  output.write('<meta http-equiv="content-type" content="text/html;charset=utf-8" />\n')
-  output.write('</head>\n')
-  output.write('<body>\n')
-  output.write('<a href="http://ccd.fnal.gov/techpubs/fermilab_spires.html">Fermilab Technical Publications</a>\n')
-  output.write('<h2>Fermilab experiments</h2>')
-  output.write('<p>Please send comments, additions or corrections ')
-  output.write('to Heath O\'Connell <a href="mailto:hoc@fnal.gov">hoc@fnal.gov</a>.</p>')
-  os.environ['TZ'] = 'CEST+5'
-  dateTimeStamp = '<i>Updated '+time.strftime('%Y-%m-%d %H:%M:%S')+'</i>\n'
-  output.write(dateTimeStamp)
-  output.write('<br />\n<table>\n')
-  output.write('<tr><td>Number</td><td>Collaboration</td><td>Spokesperson</td><td>Title (test beam in green)</td><td>Proposal</td><td>Finished</td></tr>')
-  for report in reports :
-    report.pop(0)
-    if VERBOSE: print "report =", report
-    if report[4]: 
-      search2 = '035__a:' + report[4] 
-      if VERBOSE: print "search2 =", search2
-      result = perform_request_search(p=search2,cc='HepNames')
-      if VERBOSE: print report[4], result
-      report[2] = '<a href="http://inspirehep.net/record/' + str(result[0]) + '">'+report[2]+'</a>'
-    #print report
-    line = \
-'<tr>\
-  <td><a href="http://inspirehep.net/record/'+report[1]+'">'+report[0]+'</a></td>\
-  <td>'+report[5]+'</td>\
-  <td>'+report[2]+'</td>\
-  <td>'+report[3]+'</td>\
-  <td><a href="http://inspirehep.net/record/'+report[7]+'">'+report[8]+'</a></td>\
-  <td>'+report[9]+'</td> </tr>\n'
-    if re.search(r'THESIS', report[0]):
-      if report[5]:
-        search2 = '119__a:' + report[5]
-        result = perform_request_search(p=search2,cc='collaborations')
-        if result: result = result[0]
-        collaboration = get_fieldvalues(result,'710__g')
-        if collaboration: 
-          collaboration = collaboration[0]
-          collaboration = collaboration.replace(' Collaboration','')
-          report[5] = report[5] + ' (' + collaboration + ')'
-        if result:
-          report[5] = '<a href="http://inspirehep.net/record/' + str(result) + '">'+report[5]+'</a>'
-      line = \
-'<tr>\
-  <td><a href="http://inspirehep.net/record/'+report[1]+'">'+report[0]+'</a></td>\
-  <td>'+report[2]+'</td>\
-  <td>'+report[5]+'</td>\
-  <td>'+report[6]+'</td>\
-  <td>'+report[3]+'</td>\
-</tr>\n'
-    output.write(line)
-  output.write('</table>\n')
-  output.write('</body>\n')
-  output.write('</html>\n')
-  output.close()
-  print '\\rm fermilab-reports-' + series + '.html' 
-  print 'wget http://hoc.web.cern.ch/hoc/' + 'fermilab-reports-' + series + '.html' 
+INSPIRE_URL = 'http://inspirehep.net/record/'
+PROPOSAL_URL = 'https://ccd.fnal.gov/techpubs/fermilab-reports-proposal.html'
+
+ELEMENT = ElementMaker(makeelement=html_parser.makeelement)
+
+HEADING_DICT = {'number':('119__a', '419__a'), 
+                'collaboration':'710__g',
+                'spokespersons':'702__', 
+                'title':'245__a',
+                'status':'046__t',
+                'institutions':'700__u'}
+HEADING = HEADING_DICT.keys()
+
+INDEXES = {'Institutions':'110__u', 'Experiments':'119__a',
+           'HepNames':'678__a', 'HEP':'001'}
+
+def populate_td(input, recid=None):
+
+    if recid:
+        href_link = INSPIRE_URL + recid
+        try:
+            return ELEMENT.A(input, href=href_link)
+        except ValueError:
+            print 'Problem with', input, href_link
+            quit()
+
+    ul = LH.Element("ul")  
+    for spokesperson in input:
+        name = spokesperson['name']
+        name = re.sub(r' \(.*', '', name)
+        display = name + ' (' + \
+                  spokesperson['start'] + ' - '
+        if spokesperson['curr'].lower() == 'current':
+            display += 'present)'
+            display = ELEMENT.B(display)
+        else:
+            display += spokesperson['end'] + ')'
+        ul.append(ELEMENT.LI(display))
+    return ul 
+
+def create_html_table(experiments):
+    """HTML TABLE generation by lxml.html tree.
+       Follows http://lxml.de/3.3/api/lxml.html.builder-module.html
+    """
+
+    table = LH.Element("table", attrib={"class":"sortable", 
+                                        "border":"1"})
+    table_tr = ELEMENT.TR()
+    table_tr.append(ELEMENT.TH('FNAL Number'))
+    new_heading = list(HEADING)
+    new_heading.remove('number')
+    for heading in new_heading:
+        if heading == 'institutions':
+            table_tr.append(ELEMENT.TH(heading.capitalize(), width="12%"))
+        else:
+            table_tr.append(ELEMENT.TH(heading.capitalize()))
+    table.append(table_tr)
+    for sorter, experiment in sorted(experiments.items(), reverse=True):
+        table_tr = ELEMENT.TR()
+        table_tr.append(ELEMENT.TD(populate_td(experiment['number'], 
+                                   recid=experiment['recid'])))
+        new_heading = list(HEADING)
+        new_heading.remove('number')
+        for key in new_heading:
+            #print key, experiment[key]
+            try:
+                table_tr.append(ELEMENT.TD(experiment[key]))
+            except:
+                try:
+                    table_tr.append(ELEMENT.TD(populate_td(experiment[key])))
+                except:
+                    table_tr.append(ELEMENT.TD())
+        table.append(table_tr)
+    return table
+
+
+def time_stamp():
+
+    from datetime import datetime
+    import pytz
+
+    tz = pytz.timezone('America/Chicago')
+    tz = datetime.now(tz).strftime('%Y-%m-%d %H:%M:%S')
+    tz = 'Updated:' + tz
+    return tz
+
+def create_html(experiments):
+    """Creates the html for the page."""
+
+    html = LH.Element("html")
+    body = LH.Element("body")
+    head = LH.Element("head")
+    table = create_html_table(experiments)
+    comment = ET.Comment("Fermilab Experiments")
+
+    head.append(ELEMENT.TITLE("Fermilab Experiments"))
+    body.append(ELEMENT.H1("Fermilab Experiments"))
+    body.append(ELEMENT.P(time_stamp()))
+    body.append(ELEMENT.P(ELEMENT.A("Fermilab Proposals", href=PROPOSAL_URL)))
+    body.append(comment)   
+    body.append(table)
+
+    html.append(head)
+    html.append(body)
+
+    root = LH.tostring(html)
+      
+    soup = BS(root)         #make BeautifulSoup
+    out = soup.prettify()   #prettify the html
+
+    return out
+
+
+
+def populate_experiments_dict(recid):
+    experiment = {}
+    experiment['recid'] = str(recid)
+    for key, value in HEADING_DICT.items():
+         if key == 'number':
+             numbers =  get_fieldvalues(recid, value[0])
+             numbers += get_fieldvalues(recid, value[1])             
+             for number in numbers:
+                 if number.startswith('FNAL'):
+                     experiment[key] = number
+                     sorter = re.sub(r'\D', '', number)
+         elif key == 'institutions':
+            experiment[key] = ' / '.join(sorted(set(get_fieldvalues(recid, 
+                                                                value))))
+         elif key == 'spokespersons':
+            spokes = []
+            fields = [('name', 'a'), ('id', 'i'), ('start', 'd'),
+                      ('end', 'e'), ('curr', 'z')]
+            for item in bfo(int(recid)).fields(value):
+                spoke = {}
+                for (field, element) in fields:
+                    if item.has_key(element):
+                        spoke[field] = item[element]
+                    else:
+                        spoke[field] = '???'
+                spokes.append(spoke)
+            experiment[key] = spokes
+         else:
+             try:
+                 experiment[key] = get_fieldvalues(recid, value)[0]
+             except IndexError:
+                 experiment[key] = get_fieldvalues(recid, value)
+         if key == 'status':
+             try:
+                 if experiment[key] == '9999':
+                     experiment[key] = 'Running'
+                 elif re.search(r'\d', experiment[key]):
+                     experiment[key] = 'Ended: ' + experiment[key]
+             except TypeError:
+                 experiment[key] = '???'
+         if key == 'title':
+             experiment[key] = experiment[key][:50]
+    return {sorter:experiment}
+
+def generate_experiments_dict(search):
+    experiments = {}
+    result = perform_request_search(p=search, cc='Experiments')
+    for recid in result:
+        experiments.update(populate_experiments_dict(recid))
+    return experiments
+
+def main():
+    file = 'www/fermilab-experiments.html' 
+    output = open(file, 'w')
+    experiments = generate_experiments_dict(SEARCH)
+    html = create_html(experiments)
+    output.write(html)
+    output.close()
+    print '\\rm fermilab-experiments.html' 
+    print 'wget http://hoc.web.cern.ch/hoc/fermilab-experiments.html' 
+    print ''
+    print 'http://ccd.fnal.gov/techpubs/fermilab-experiments.html'
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print 'Exiting'
 
