5775e0d8688c2dcc5caa637d1bcab8cdb5c5318a
Heath O'Connell <hoc3426@gmail.com>
2019-05-01 22:06:56 +0200

    Removed print statement

--- hep_collaboration_authors.py
+++ hep_collaboration_authors.py
@@ -254,7 +254,6 @@ def create_xml(eprint=None, doi=None, author_dict=None):
                 continue
             #elif re.match(r"^0000-0", affiliation):
             elif re.search(r"0000-0", affiliation):
-                print affiliation
                 try:
                     orcid = re.search(r'(0000-\d{4}-\d{4}-\d{3}[\dX])',
                                   affiliation).group(1)
