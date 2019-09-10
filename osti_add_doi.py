cdef04a706f21b0a6e307e8a28465df7650ec485
Heath O'Connell <hoc3426@gmail.com>
2019-09-10 21:12:41 +0200

    osti_add_doi: Reverted search to all Fermilab

--- osti_add_doi.py
+++ osti_add_doi.py
@@ -18,7 +18,7 @@ SEARCH = r'035__9:osti du:2019 -0247:doi -037:arXiv -773__p:/\w/'
 RESULT_HEP = perform_request_search(p=SEARCH, cc='HEP')
 RESULT_FERMILAB = perform_request_search(p=SEARCH, cc='Fermilab')
 RESULT = intbitset(RESULT_HEP) | intbitset(RESULT_FERMILAB)
-RESULT = [1253262]
+#RESULT = [1253262]
 print "Number of records:", len(RESULT)
 RESULT = reversed(RESULT)
 OSTI_IDS = set()
