0dd4b61ebae75e9f9fc2144f43af5af4d2df96f0
Heath O'Connell <hoc3426@gmail.com>
2020-01-08 17:24:33 +0100

    hep_convert_email_to_id_input: added

---
+++ hep_convert_email_to_id_input.py
@@ -0,0 +1,16 @@
+VERBOSE = False
+#VERBOSE = True
+
+RECIDS = None
+
+SEARCH = 'find dadd 2015 or 2016 or 2017 and r fermilab not ac 10+'
+SEARCH = 'find dadd 2015 and r fermilab not ac 10+'
+SEARCH = 'find ea C.T.Hill.1'
+SEARCH = 'find r fermilab not ac 10+'
+SEARCH = 'find dadd 2015 or 2016 or 2017 or 2018 and r fermilab not ac 10+'
+
+SEARCH = '100__m:/fnal.gov/ or 700__m:/fnal.gov/'
+SEARCH = '100__m:/\w/ or 700__m:/\w/ du:2019-02-25->2019-04-01'
+SEARCH = '100__m:/\w/ or 700__m:/\w/ du:2019-03-15->2019-04-01'
+SEARCH = None
+
